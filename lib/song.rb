6bfbf038227d9ce4409815cf978a5553ac7496d3
Jose Aguilar <jaguilarny@gmail.com>
2022-09-09 18:33:30 -0400

    commit tree 2d29e8f55716fc7566673dcf3ae505214d371da8

--- lib/song.rb
+++ lib/song.rb
@@ -1,29 +1,17 @@
-require 'pry'
+require "pry"
 
 class Song
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
   def self.all
     @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def artist=(artist)
@@ -31,6 +19,6 @@ class Song
   end
 
   def to_param
-    name.downcase.gsub(' ', '-')
+    name.downcase.gsub(" ", "-")
   end
 end
